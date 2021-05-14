f43388b894b206c1e09ccb48bcc5e292617bbc8f
michal <michal.wilk0@yahoo.com>
2021-05-14 21:43:08 +0200

    added example scheme

--- bearer/app/db/citizen.py
+++ bearer/app/db/citizen.py
@@ -1,21 +1,33 @@
 from typing import Optional
-from app.models.citizen_model import CitizenModel
+from app.models.citizen_model import CitizenDriveData, CitizenModel
 from .collections import citizen_collection
+import json
 
-async def create_citizen(citizen_model:CitizenModel):
-    citizen_collection.insert_one(citizen_model)
 
-async def read_citizen(citizen_id:str) -> Optional[CitizenModel]:
-    result = await citizen_collection.find_one({"PID" : citizen_id})
+async def create_citizen(citizen_model: CitizenModel) -> bool:
+    doc = await citizen_collection.find_one({"PID": citizen_model.PID})
+    if doc is not None:
+        return False
+    cit_json = json.loads(citizen_model.json())
+    await citizen_collection.insert_one(cit_json)
+    return True
+
+
+async def read_citizen(citizen_id: str) -> Optional[CitizenModel]:
+    result = await citizen_collection.find_one({"PID": citizen_id})
     if result is None:
         return None
     else:
         return CitizenModel(**result)
 
-async def update_mail(citizen_id:str, mail:str) -> bool:
-    result = citizen_collection.update_one({"PID" : citizen_id}, {"$set" : {"contact_mail" : mail}})
+
+async def update_mail(citizen_id: str, mail: str) -> bool:
+    result = citizen_collection.update_one(
+        {"PID": citizen_id}, {"$set": {"contact_mail": mail}}
+    )
     return result.modified_count == 1
 
-async def delete_citizen(citizen_id:str) -> bool:
-    result = citizen_collection.delete_one({"PID" : citizen_id})
+
+async def delete_citizen(citizen_id: str) -> bool:
+    result = citizen_collection.delete_one({"PID": citizen_id})
     return result.deleted_count == 1
