592295ee5a9b799f22f170210e753e91a1bcc354
michal <michal.wilk0@yahoo.com>
2021-05-14 11:17:40 +0200

    added basic authorization

--- bearer/app/routes/citizen.py
+++ bearer/app/routes/citizen.py
@@ -1,7 +1,9 @@
-from app.models.citizen_model import CitizenModel
-from fastapi import APIRouter, Depends, Body
+from app.models.citizen_model import CitizenMailAuth
+from app.models import CitizenModel
+from fastapi import APIRouter, Depends, Body, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from app import db
+from app.db import citizen
+from app.db.auth import authorize_authority
 from typing import Optional
 
 
@@ -10,21 +12,31 @@ security = HTTPBasic()
 
 @citizen_router.get('')
 async def get_citizen(citizen_id:str, credentials:HTTPBasicCredentials=Depends(security)) -> Optional[CitizenModel]:
-    return await db.citizen.read_citizen(citizen_id)
+    if await authorize_authority(credentials.username, credentials.password):
+        raise HTTPException(status_code=501, detail="not authorized")
+
+    return await citizen.read_citizen(citizen_id)
 
 
 @citizen_router.post('')
 async def create_citizen(citizen_model:CitizenModel, credentials:HTTPBasicCredentials=Depends(security)): 
-    success = await db.citizen.create_citizen(citizen_model)
-    return {"successful" : success}
+    if await authorize_authority(credentials.username, credentials.password):
+        raise HTTPException(status_code=501, detail="not authorized")
+
+    success = await citizen.create_citizen(citizen_model)
+    return {"success" : success}
 
 
 @citizen_router.delete('')
 async def delete_citizen(citizen_id:str, credentials:HTTPBasicCredentials=Depends(security)):
-    return db.citizen.delete_citizen(citizen_id)
+    if await authorize_authority(credentials.username, credentials.password):
+        raise HTTPException(status_code=501, detail="Bad credentials")
+
+    return citizen.delete_citizen(citizen_id)
 
 
 @citizen_router.put('/mail')
-async def update_mail(citizen_id:str, credentials:HTTPBasicCredentials=Depends(security), mail:str=Body(...)):
-    return db.citizen.update_mail(citizen_id, mail)
+async def update_mail(citizen_auth_model:CitizenMailAuth):
+    # TODO: tutaj wysłany bedzie mail!!!
+    return citizen.update_mail(citizen_auth_model.PID, citizen_auth_model.name)
 
