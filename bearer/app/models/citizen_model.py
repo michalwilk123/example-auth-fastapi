65f350e18f8e72a9518e2d436d7c704c515d0eef
michal <michal.wilk0@yahoo.com>
2021-05-14 11:18:13 +0200

    added citizen validation

--- bearer/app/models/citizen_model.py
+++ bearer/app/models/citizen_model.py
@@ -1,20 +1,30 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from datetime import date
+from fastapi import Query
 
 
 class CitizenModel(BaseModel):
     """
-    Model whitch defines the citizen
+    Model whitch defines the citizen, this will be
+    actually stored in the mongodb database
     """
-    name:str
-    surname:str
-    PID: str
-    ID_card_number: str
-    martial_status: Optional[str]
-    driver_license_id: Optional[str]
-    driver_license_exp_date: Optional[str]
-    driver_penalty_points: Optional[str]
-    place_of_birth: str
-    residence: str
-    contact_number: Optional[str]
-    contact_mail: Optional[str]
+    name:str = Query(..., regex="[a-zA-Z]{3,40}")
+    surname:str = Query(..., regex="[a-zA-Z]{3,40}")
+    PID: str = Query(..., regex="[0-9]{11}")
+    ID_card_number: str = Query(..., regex="[A-Z0-9]{11}")
+    martial_status: Optional[str] = Query(None, max_length=50)
+    driver_license_id: Optional[str] = Query(None, regex="[A-Z0-9]{11}")
+    driver_license_exp_date: Optional[date]
+    driver_penalty_points: Optional[str] = Query(None, regex="[0-9]{0,3}")
+    place_of_birth: str = Query(..., regex="[A-Za-z ]{2,100}")
+    residence: str = Query(..., regex="[A-Za-z ]{2,100}")
+    contact_number: Optional[str] = Query(..., regex="[0-9]{6,20}")
+    contact_mail: Optional[EmailStr]
+
+
+class CitizenMailAuth(BaseModel):
+    name:str = Query(..., regex="[a-zA-Z]{3,40}")
+    surname:str = Query(..., regex="[a-zA-Z]{3,40}")
+    PID: str = Query(..., regex="[0-9]{11}")
+    contact_mail: Optional[EmailStr]
